aef92456ab3b416e3e874866dfbec2f425ae73e0
mjtsolo <91265190+mjtsolo@users.noreply.github.com>
2021-10-07 14:42:12 +0200

    Add files via upload

---
+++ Task 0.5.py
@@ -0,0 +1,8 @@
+a = 6
+b = 8
+c = 10
+area = (a+b+c)/2
+print(area)
+
+
+
---
+++ Task 0.7.py
@@ -0,0 +1,3 @@
+celsius = 32
+fahrenheit = (celsius *1.8) +32
+print("%.2f celsius) = %.2f Fahrenheit" %(celsius,fahrenheit))
